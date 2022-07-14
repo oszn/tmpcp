e18c5ce1f029c0151900020e493e9931fef19dcb
oszn <oszn>
2022-07-14 13:54:10 +0800

    fix the bug

---
+++ src/main/java/com/example/demo/service/test.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+public class test {
+    void hello(){
+        String s[]=new String[10];
+        int a=-1;
+    }
+
+    public static void main(String[] args) {
+        test t=new test();
+        t.hello();
+    }
+}
